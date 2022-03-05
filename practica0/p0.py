97041bc2784ed3c6ed18e727967d6bef9baa36a8
Valentino Lugli <valentinolugli@gmail.com>
2022-03-05 21:52:15 +0100

    [P0][99%] Excersice 3 & 4 completed.
    Notes:
    	- Excersise 4 needs to be checked, something weird happening with the pyramid render.

--- practica0/p0.py
+++ practica0/p0.py
@@ -30,6 +30,9 @@ from sklearn.datasets import load_iris
 # - Importando Matplotlib
 import matplotlib.pyplot as plt
 
+# - Importando para hacer uso de funciones matemáticas más complejas.
+import math
+
 # FUNCIONES AUXILIARES
 
 # FIN FUNCIONES AUXILIARES
@@ -38,10 +41,88 @@ import matplotlib.pyplot as plt
 
 ## FUNCIONES EJER 2
 
-def trainTestSplit(x_input, y_input, split):
-    pass    
+def trainTestSplit(x_input, y_input, split, className = [None]):
     
-    # return x_train, y_train, x_test, y_test
+    # - Obtener las clases del vector de salida, son los números únicos
+    # que están, se obtienen con la función 'unique'.
+    classes = np.unique(y_input)
+        
+    # - Se obtiene el tamaño final de los vectores de test y train,
+    # en proporción.
+    # - Para que no haya problemas con el redondeo, se utiliza floor.
+    # y el restante para la otra clase.
+    
+    numTrain = math.floor(len(x_input) * split)
+    numTest = len(x_input) - numTrain
+    
+    # Se generan los arrays que contendrán los datos divididos.
+    x_train = np.zeros( (numTrain, x_input.shape[1]) )
+    x_test =  np.zeros( (numTest, x_input.shape[1]) )
+    
+    # Ídem para el vector de las etiquetas.
+    y_train = np.zeros( (numTrain,) )
+    y_test = np.zeros( (numTest,) )
+    
+    # Por cada clase...
+    for i in classes:
+        # Se obtienen los datos de entrada que corresponden a la clase
+        # i-ésima.
+        tempClass = x_input[ np.where(y_labels == i) ]
+        
+        # Se realiza un barajeo de los datos para desordenarlos dentro de su
+        # propia clase.
+        np.random.shuffle(tempClass)
+        
+        # Se obtiene la proporción de datos que irán al conjunto de train y
+        # test dentro de la clase.
+        trainDiv = math.floor(len(tempClass) * split)
+        testDiv = len(tempClass) - trainDiv
+        
+        # Se calcula el desplazamiento para añadir los datos a los arrays
+        # divididos, en este momento estarán en orden por cada clase.
+        
+        # Después que termine el bucle se volverán a barajear.
+        
+        startTrain = i * trainDiv
+        endTrain = startTrain + trainDiv
+        
+        startTest = i * testDiv
+        endTest = startTest + testDiv
+
+        # Como los datos han sido ya barajeados in-place, se puede obtener
+        # los primeros datos hasta la proporción requerida y se añaden al
+        # conjunto de entrenamiento, y el resto al conjunto de test comodamente
+        # pues ya no están en orden.
+        
+        x_train[startTrain:endTrain] = tempClass[:trainDiv]
+        x_test[startTest:endTest] = tempClass[trainDiv:]
+        
+        # Como el bucle es por cada clase, a cada dato le corresponderá la 
+        # clase i-ésima.
+        
+        y_train[startTrain:endTrain] = i
+        y_test[startTest:endTest] = i
+        
+        if className[0] != None:
+            
+            print("--- Clase",className[i],"---")
+            print("Ejemplos train:", len(tempClass[:trainDiv]))
+            print("Ejemplos test:", len(tempClass[trainDiv:]))
+        
+    # Se obtienen índices con una permutación.
+    permutTrain = np.random.permutation(numTrain)
+    permutTest = np.random.permutation(numTest)
+    
+    # Se desordenan nuevamente los elementos pero sin perder la correspondencia
+    # entre x e y.
+    
+    x_train = x_train[permutTrain]
+    y_train = y_train[permutTrain]
+    
+    x_test = x_test[permutTest]
+    y_test = y_test[permutTest]
+    
+    return x_train, y_train, x_test, y_test
 
 ## FUNCIONES EJER 2 FIN
 
@@ -110,10 +191,93 @@ plt.show()
 # conservando la proporción de elementos en cada clase tanto en training 
 # como en test.
 
+iris = load_iris()
 
+x_feat = iris.data
+y_labels = iris.target
+
+x_train, y_train, x_test, y_test = trainTestSplit(x_feat, y_labels, 0.8, 
+                                                  iris.target_names)
+
+print("Clase de los ejemplos de entrenamiento:", y_train)
+print("Clase de los ejemplos de test:", y_test)
 
 #%% EJERCICIO 3
 ###############
 
+# 3.1: Obtener 100 valores equiespaciados entre 0 y 4pi
+values = np.linspace(0, math.pi * 4, 100)
+
+# 3.2: Obtener el valor de 10^−5 · sinh(x), cos(x) y 
+# tanh(2 · sin(x) − 4 · cos(x)) para los 100 valores anteriormente calculados.
+
+hyperSine = math.pow(10, -5) * np.sinh(values)
+cosine = np.cos(values)
+hyperTanSinCos = np.tanh(2 * np.sin(values) - 4 * np.cos(values))
+
+# 3.3: Visualizar las tres curvas simultáneamente en el mismo plot
+
+plt.figure()
+plot = plt.plot(values, hyperSine, 'g--', values, cosine, 'k--', values, hyperTanSinCos, 'r--')
+plt.legend(labels=["y = 1e-5 * sinh(x)", "y = cos(x)", "y = tanh(2sin(x) - 4cos(x)"], fontsize=8)
+plt.show()
+
 #%% EJERCICIO 4
 ###############
+
+# 4.1: Funciones
+
+# - f(x, y) = 1 - |x + y| - |y - x|
+x1 = np.arange(-6, 6, 0.25)
+y1 = np.arange(-6, 6, 0.25)
+
+x1, y1 = np.meshgrid(x1, y1)
+
+z1 = 1 - np.abs(x1 + y1) - np.abs(y1 - x1)
+    
+# - f(x, y) = x * y * e^(-x^2-y^2)
+x2 = np.arange(-2, 2, 0.0625)
+y2 = np.arange(-2, 2, 0.0625)
+
+x2, y2, = np.meshgrid(x2, y2)
+
+z2 = x2 * y2 * np.exp(-np.power(x2, 2)-np.power(y2,2))
+
+# 4.2: Dibujar en 3D las funciones calculadas.
+
+# - Generar la figura, se añade el tamaño en pulgadas y su DPI.
+fig = plt.figure(figsize=(15, 10), dpi=300)
+
+# - Como son dos figuras, se añade dos subplots, se indica con el 2. 
+# - Como es la  primera figura, se utiliza el 1 al final.
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+
+# - Se dibuja la figura con los elementos calculados.
+surf = ax.plot_surface(x1, y1, z1, rstride=1, cstride=1, cmap=plt.cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+# - Se delimita la figura por sus lados.
+ax.set_zlim(-10, 0)
+ax.set_xlim(-7, 7)
+ax.set_ylim(-7, 7)
+
+# - Añadiendo el título.
+plt.title("Pirámide")
+
+# - Se añade el segundo subplot, se indica ahora con el 2.
+ax = fig.add_subplot(1, 2, 2, projection='3d')
+
+# - Se dibuja, se cambia el mapa de color a viridis.
+surf = ax.plot_surface(x2, y2, z2, rstride=1, cstride=1, cmap=plt.cm.viridis,
+                       linewidth=0, antialiased=False)
+
+# - Se delimitan los ejes.
+ax.set_zlim(-0.15, 0.15)
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+
+# - Titulo
+plt.title("$x \cdot y \cdot e^{-x^2-y^2}$")
+
+# - Se dibuja la figura entera.
+plt.show()
