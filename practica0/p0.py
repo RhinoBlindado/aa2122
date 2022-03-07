83a78e301fdb49f2a96910856ad3be04bcc8cb24
Valentino Lugli <valentinolugli@gmail.com>
2022-03-07 18:50:17 +0100

    [P0][100%] Finished comments.

--- practica0/p0.py
+++ practica0/p0.py
@@ -15,35 +15,60 @@
     February-March 2022
 """
 
+# NOTA: En Spyder se tienen las celdas, demarcadas con un comentario de la
+#       forma '#%%' que permite ejecutar una sección de código independiente
+#       del resto. 
+#       Esta práctica está realizada de manera que cada ejercicio está
 
 # LIBRERIAS
 
-# - Importando Numpy
+# - Importando Numpy.
 import numpy as np
 
-# - Importando scikit-learn general
-import sklearn as sk
-
-# - Importanto el dataset de Iris
+# - Importanto el dataset de Iris.
 from sklearn.datasets import load_iris
 
-# - Importando Matplotlib
+# - Importando Matplotlib.
 import matplotlib.pyplot as plt
 
 # - Importando para hacer uso de funciones matemáticas más complejas.
 import math
-
-# FUNCIONES AUXILIARES
-
-# FIN FUNCIONES AUXILIARES
 
 # FUNCIONES PARA EJERCICIOS
 
 ## FUNCIONES EJER 2
 
 def trainTestSplit(x_input, y_input, split, className = [None]):
+    """
+    Realizar la división de los datos en Entrenamiento y Test dado una
+    proporcion entr 0 y 1.
+
+    Parameters
+    ----------
+    x_input : Numpy Array
+        Datos de entrada
+    y_input : Numpy Array
+        Etiquetas de los datos de entrada.
+    split : Float
+        Proporción de la división
+    className : Python List, optional
+        Para imprimir los datos por pantalla, por defecto es [None] y 
+        no se imprime.
+
+    Returns
+    -------
+    x_train : Numpy Array
+        Datos de entrenamiento.
+    y_train : Numpy Array
+        Etiquetas de los datos de entrenamiento.
+    x_test : Numpy Array
+        Datos de test.
+    y_test : Numpy Array
+        Etiquetas de los datos de test.
+
+    """
     
-    # - Obtener las clases del vector de salida, son los números únicos
+    # - Obtener las clases del vector de etiquetas, son los números únicos
     # que están, se obtienen con la función 'unique'.
     classes = np.unique(y_input)
         
@@ -51,11 +76,10 @@ def trainTestSplit(x_input, y_input, split, className = [None]):
     # en proporción.
     # - Para que no haya problemas con el redondeo, se utiliza floor.
     # y el restante para la otra clase.
-    
     numTrain = math.floor(len(x_input) * split)
     numTest = len(x_input) - numTrain
     
-    # Se generan los arrays que contendrán los datos divididos.
+    # Se generan los arrays de ceros que contendrán los datos divididos.
     x_train = np.zeros( (numTrain, x_input.shape[1]) )
     x_test =  np.zeros( (numTest, x_input.shape[1]) )
     
@@ -79,8 +103,7 @@ def trainTestSplit(x_input, y_input, split, className = [None]):
         testDiv = len(tempClass) - trainDiv
         
         # Se calcula el desplazamiento para añadir los datos a los arrays
-        # divididos, en este momento estarán en orden por cada clase.
-        
+        # x_train y x_test, en este momento estarán en orden por cada clase.
         # Después que termine el bucle se volverán a barajear.
         
         startTrain = i * trainDiv
@@ -91,8 +114,8 @@ def trainTestSplit(x_input, y_input, split, className = [None]):
 
         # Como los datos han sido ya barajeados in-place, se puede obtener
         # los primeros datos hasta la proporción requerida y se añaden al
-        # conjunto de entrenamiento, y el resto al conjunto de test comodamente
-        # pues ya no están en orden.
+        # conjunto de entrenamiento, y el resto al conjunto de test 
+        # pues ya no están en el orden original.
         
         x_train[startTrain:endTrain] = tempClass[:trainDiv]
         x_test[startTest:endTest] = tempClass[trainDiv:]
@@ -103,13 +126,15 @@ def trainTestSplit(x_input, y_input, split, className = [None]):
         y_train[startTrain:endTrain] = i
         y_test[startTest:endTest] = i
         
+        # Si se ingresa una lista, se pone en modo verbose.
         if className[0] != None:
             
             print("--- Clase",className[i],"---")
             print("Ejemplos train:", len(tempClass[:trainDiv]))
             print("Ejemplos test:", len(tempClass[trainDiv:]))
         
-    # Se obtienen índices con una permutación.
+    # Se obtienen índices con una permutación para volver a barajear los 
+    # datos otra vez.
     permutTrain = np.random.permutation(numTrain)
     permutTest = np.random.permutation(numTest)
     
@@ -161,26 +186,32 @@ x_featReduced = x_feat[:,::2]
 #      color diferente e indicando con una leyenda la clase a la que 
 #      corresponde cada color. 
 
-# - Creando una figura
+# Creando una figura
 plt.figure()
 
-# - Realizando el scatter plot con los datos indicados, 'x' es la primera 
-#   característica y 'y' la tercera.
-# - 'c' indica la clase a la que pertenece cada dupla (x,y).
-# - cmap indica como colorear las distintas clases.
+# Haciendo una lista de los colores para el Scatter Plot.
+colors = ['r', 'g', 'b']
 
-scatter = plt.scatter(x_featReduced[:, 0], x_featReduced[:, 1], c=y_labels,
-                       cmap='Set1')
+# Por cada etiqueta del dataset...
+for i, label in enumerate(iris.target_names.tolist()):
+    # ... obtener 'x' e 'y' para cada etiqueta, para eso se utiliza np.where
+    # para obtener los indices que tienen las etiquetas i-ésimas, luego se
+    # se asigna la columna 0 o 1 a la variable correspondiente de los datos
+    # que tienen esa etiqueta...
+    x = x_featReduced[ np.where(y_labels == i) ][:, 0]
+    y = x_featReduced[ np.where(y_labels == i) ][:, 1]
+    
+    # ... pintar el Scatter Plot con los datos actuales.
+    plt.scatter(x, y, c=colors[i], label=label)
+    
+# Se imprime la leyenda, ya se infiere de los datos que tiene el Scatter Plot.
+plt.legend()
 
 # Renombrando los ejes de la figura acorde a los datos.
 plt.xlabel(iris.feature_names[0])
 plt.ylabel(iris.feature_names[2])
 
-# - Añadiendo las legendas correspondientes, se obtienen los datos del scatter
-#   plot sobre las clases y se añade con 'labels' los nombres correspondientes.
-# - Se utiliza '.tolist()' porque las arrays de Numpy generan un error con
-#   Matplotlib.
-plt.legend(handles=scatter.legend_elements()[0], labels=iris.target_names.tolist())
+# Mostrar la figura.
 plt.show()
 
 
@@ -191,14 +222,19 @@ plt.show()
 # conservando la proporción de elementos en cada clase tanto en training 
 # como en test.
 
+# Cargar el dataset nuevamente.
 iris = load_iris()
 
+# Obtener los datos y sus etiquetas.
 x_feat = iris.data
 y_labels = iris.target
 
+# Llamar a la función personalizada que divide el dataset con 80/20 datos
+# para entrenamiento y test.
 x_train, y_train, x_test, y_test = trainTestSplit(x_feat, y_labels, 0.8, 
                                                   iris.target_names)
 
+# Imprimir por pantalla los datos.
 print("Clase de los ejemplos de entrenamiento:", y_train)
 print("Clase de los ejemplos de test:", y_test)
 
@@ -211,14 +247,20 @@ values = np.linspace(0, math.pi * 4, 100)
 # 3.2: Obtener el valor de 10^−5 · sinh(x), cos(x) y 
 # tanh(2 · sin(x) − 4 · cos(x)) para los 100 valores anteriormente calculados.
 
+#    10^−5 · sinh(x)
 hyperSine = math.pow(10, -5) * np.sinh(values)
+#   cos(x)
 cosine = np.cos(values)
+#   tanh(2 · sin(x) − 4 · cos(x)) 
 hyperTanSinCos = np.tanh(2 * np.sin(values) - 4 * np.cos(values))
 
 # 3.3: Visualizar las tres curvas simultáneamente en el mismo plot
-
 plt.figure()
+
+# Se tiene la tupla (x, y, formatoLinea), se indica '--' para las lineas 
+# punteadas con los colores que se piden.
 plot = plt.plot(values, hyperSine, 'g--', values, cosine, 'k--', values, hyperTanSinCos, 'r--')
+# Se inserta la leyenda en orden con las tuplas anteriores.
 plt.legend(labels=["y = 1e-5 * sinh(x)", "y = cos(x)", "y = tanh(2sin(x) - 4cos(x)"], fontsize=8)
 plt.show()
 
@@ -227,57 +269,56 @@ plt.show()
 
 # 4.1: Funciones
 
-# - f(x, y) = 1 - |x + y| - |y - x|
-x1 = np.arange(-6, 6, 0.25)
-y1 = np.arange(-6, 6, 0.25)
+# f(x, y) = 1 - |x + y| - |y - x|
+#   Obteniendo los datos en los intervalos pedidos.
+x1 = np.linspace(-6, 6, 50)
+y1 = np.linspace(-6, 6, 50)
 
+#   Obtener una malla con los datos
 x1, y1 = np.meshgrid(x1, y1)
 
+#   Obtener el cálculo pedido.
 z1 = 1 - np.abs(x1 + y1) - np.abs(y1 - x1)
     
-# - f(x, y) = x * y * e^(-x^2-y^2)
-x2 = np.arange(-2, 2, 0.0625)
-y2 = np.arange(-2, 2, 0.0625)
+# f(x, y) = x * y * e^(-x^2-y^2)
+#   Ídem con el cálculo anterior.
+x2 = np.linspace(-2, 2, 50)
+y2 = np.linspace(-2, 2, 50)
 
-x2, y2, = np.meshgrid(x2, y2)
+x2, y2 = np.meshgrid(x2, y2)
 
 z2 = x2 * y2 * np.exp(-np.power(x2, 2)-np.power(y2,2))
 
 # 4.2: Dibujar en 3D las funciones calculadas.
 
-# - Generar la figura, se añade el tamaño en pulgadas y su DPI.
+# Generar la figura, se añade el tamaño en pulgadas y su DPI.
 fig = plt.figure(figsize=(15, 10), dpi=300)
 
-# - Como son dos figuras, se añade dos subplots, se indica con el 2. 
+# - Como son dos figuras, se añaden dos subplots, se indica con el 2. 
 # - Como es la  primera figura, se utiliza el 1 al final.
 ax = fig.add_subplot(1, 2, 1, projection='3d')
 
-# - Se dibuja la figura con los elementos calculados.
+# Se dibuja la figura con los elementos calculados.
 surf = ax.plot_surface(x1, y1, z1, rstride=1, cstride=1, cmap=plt.cm.coolwarm,
                        linewidth=0, antialiased=False)
 
-# - Se delimita la figura por sus lados.
-ax.set_zlim(-10, 0)
-ax.set_xlim(-7, 7)
-ax.set_ylim(-7, 7)
-
-# - Añadiendo el título.
+# Añadiendo el título.
 plt.title("Pirámide")
 
-# - Se añade el segundo subplot, se indica ahora con el 2.
+# Se añade el segundo subplot, se indica ahora con el 2.
 ax = fig.add_subplot(1, 2, 2, projection='3d')
 
-# - Se dibuja, se cambia el mapa de color a viridis.
+# Se dibuja, se cambia el mapa de color a viridis.
 surf = ax.plot_surface(x2, y2, z2, rstride=1, cstride=1, cmap=plt.cm.viridis,
                        linewidth=0, antialiased=False)
 
-# - Se delimitan los ejes.
+# Se delimitan los ejes para que se vea más grande la figura.
 ax.set_zlim(-0.15, 0.15)
 ax.set_xlim(-2, 2)
 ax.set_ylim(-2, 2)
 
-# - Titulo
+# Titulo
 plt.title("$x \cdot y \cdot e^{-x^2-y^2}$")
 
-# - Se dibuja la figura entera.
+# Se dibuja la figura entera.
 plt.show()
