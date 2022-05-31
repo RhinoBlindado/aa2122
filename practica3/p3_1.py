1040adc88f12605e2d2c176713af06b0fd405d37
Valentino Lugli <valentinolugli@gmail.com>
2022-05-31 22:38:49 +0200

    [P3][55%] Classification done, regression WIP
    Changes:
    	- Completed classification problem, still need to add some
    	stats to the data and check the error measures.
    	- Regression task is on the way, need to apply PCA and
    	select some error measure to do the GridSearchCV

--- practica3/p3_1.py
+++ practica3/p3_1.py
@@ -22,30 +22,150 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import pandas as pd
+import sklearn.linear_model as sklm
+import sklearn.preprocessing as skpp
+import sklearn.model_selection as skms
+import sklearn.metrics as skm
 
 # FIN FUNCIONES PARA EJERCICIOS
 
 # IMPLEMENTACION EJERCICIOS
 
+def encodeCycle(data, col, maxVal):
+    data[col+"_sin"] = np.sin(2 * np.pi * data[col] / maxVal)
+    data[col+"_cos"] = np.cos(2 * np.pi * data[col] / maxVal)
+        
+def monthToInt(data, col, monthDict):
+    data[col] = data[col].map(monthDict)
+
 def loadData(dataPath):
     dataset = pd.read_csv(dataPath, sep=";", header=0)
-    dataset = pd.get_dummies
+    return dataset
+
+def processData(data, categorical, binary, cyclical, custom):
+    data = pd.get_dummies(data, columns=categorical, drop_first=True)
+
+    for feat in binary:
+        data[feat] = np.where(data[feat] == 'yes', 1, -1)
+        
+    for feat in cyclical:
+        encodeCycle(data, feat, cyclical[feat])
+        data.drop(labels=[feat], axis=1, inplace=True)
+
+    for feat in custom:
+        for key in feat:
+            data[key] = data[key].map(feat.get(key))
     
+    return data
 
-#%% EJERCICIO 1
-#############
+def charsTagsSplit(data, tag):
+    x = data.drop([tag], axis=1)
+    y = data[tag]
+    
+    return x, y
 
-categorical = ['job', 'marital', 'education', 'contact', 'poutcome']
+def normalizeData(train, test, normCols, featRange=((-1, 1))):
+    scaler = skpp.MinMaxScaler(feature_range=featRange)
+
+    scaler.fit(train[normCols])
+    train[normCols] = scaler.transform(train[normCols])
+    test[normCols] = scaler.transform(test[normCols])
+
+
+def plotRoc(tpr, fpr, thr):
+    plt.figure()
+    plt.plot(tpr)
+
+#%% CARGA DE DATOS
+##################
+
+dataPath = "./datos/clasificacion/bank-full.csv"
+
+bankData = loadData(dataPath)
+
+
+#%% ANALISIS DE LOS DATOS
+#########################
+
+
+
+
+
+#%% CODIFICAR LOS DATOS
+#######################
+
+categorical = ['job', 'marital', 'education', 'contact']
+customMap = [{"poutcome" : { 'unknown' : 0, 'other' : 0, 'failure' : -1, 'success' : 1 }}]
 binary = ['default', 'housing', 'loan', 'y']
+cyclical = {'day' : 31, 'month' : 12}
 
-dataPath = "./datos/clasificacion/bank.csv"
+monthDict = {'jan' : 1, 'feb' : 2, 'mar' : 3, 'apr' : 4, 'may' : 5,
+             'jun' : 6, 'jul' : 7, 'aug' : 8, 'sep' : 9, 'oct' : 10,
+             'nov' : 11, 'dec' : 12}
 
-# xFeats, yTags = loadData(dataPath)
+monthToInt(bankData, "month", monthDict)
 
-dataset = pd.read_csv(dataPath, sep=";", header=0)
-dataset = pd.get_dummies(dataset, columns=categorical, drop_first=True)
+bankData.drop(labels=["duration"], axis=1, inplace=True)
+problemData = processData(bankData, categorical, binary, cyclical, customMap)
 
-for feat in binary:
-    dataset[feat] = np.where(dataset[feat] == 'yes', 1, -1)
+#%% PREPROCESADO DE DATOS
+#########################
+
+X, Y = charsTagsSplit(problemData, 'y')
+
+train_chars, test_chars, train_tags, test_tags = skms.train_test_split(X, 
+                                                                       Y, 
+                                                                       test_size=0.3,
+                                                                       stratify=True,
+                                                                       random_state=16)
+
+normalize = ["age", "balance", "pdays", "campaign", "previous"]
+normalizeData(train_chars, test_chars, normalize)
+
+train_x = train_chars.to_numpy()
+train_y = train_tags.to_numpy()
+test_x = test_chars.to_numpy()
+test_y = test_tags.to_numpy()
+
+#%% VALIDACION CRUZADA
+######################
+
+CVParamGrid =   [{
+                    'loss' : ['log'],
+                    'class_weight': [None, 'balanced'],
+                    'alpha': [0.0001, 0.001, 0.01],
+                    'eta0' : [0.01, 0.1, 0.3],
+                    'learning_rate' : ['adaptive','optimal'],
+                }]
 
 
+scores = ["accuracy", "precision", "recall", "average_precision"]
+
+RLGrid = skms.GridSearchCV(sklm.SGDClassifier(), 
+                       CVParamGrid, 
+                       scoring=scores,
+                       refit="average_precision",
+                       cv=10,
+                       verbose=1,
+                       n_jobs=-1)
+
+RLGrid.fit(train_x, train_y)
+
+
+
+#%% ENTRENAMIENTO
+#################
+bestRL = RLGrid.best_estimator_
+
+bestRL.fit(train_x, train_y)
+
+#%% TEST
+########
+
+
+#%% ANALISIS DE RESULTADOS
+##########################
+pred_test_y = bestRL.predict(test_x)
+
+print(skm.classification_report(test_y, pred_test_y))
+    
--- practica3/p3_2.py
+++ practica3/p3_2.py
@@ -21,5 +21,84 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import pandas as pd
+import sklearn.linear_model as sklm
+import sklearn.preprocessing as skpp
+import sklearn.model_selection as skml
+import sklearn.metrics as skm
+
+# FIN FUNCIONES PARA EJERCICIOS
+
+# IMPLEMENTACION EJERCICIOS
+
+def encodeCycle(data, col, maxVal):
+    data[col+"_sin"] = np.sin(2 * np.pi * data[col] / maxVal)
+    data[col+"_cos"] = np.cos(2 * np.pi * data[col] / maxVal)
+        
+def monthToInt(data, col, monthDict):
+    data[col] = data[col].map(monthDict)
+
+def loadData(dataPath, sepIn=";", headerIn=None):
+    dataset = pd.read_csv(dataPath, sep=sepIn, header=headerIn)
+    return dataset
+
+def processData(data, categorical, binary, cyclical, custom):
+    data = pd.get_dummies(data, columns=categorical, drop_first=True)
+
+    for feat in binary:
+        data[feat] = np.where(data[feat] == 'yes', 1, -1)
+        
+    for feat in cyclical:
+        encodeCycle(data, feat, cyclical[feat])
+        data.drop(labels=[feat], axis=1, inplace=True)
+
+    for feat in custom:
+        for key in feat:
+            data[key] = data[key].map(feat.get(key))
+    
+    return data
+
+def charsTagsSplit(data, tag):
+    x = data.drop([tag], axis=1)
+    y = data[tag]
+    
+    return x, y
+
+def normalizeData(train, test, normCols, featRange=((-1, 1))):
+    scaler = skpp.MinMaxScaler(feature_range=featRange)
+
+    scaler.fit(train[normCols])
+    train[normCols] = scaler.transform(train[normCols])
+    test[normCols] = scaler.transform(test[normCols])
 
 
+def plotRoc(tpr, fpr, thr):
+    plt.figure()
+    plt.plot(tpr)
+
+
+#%% CARGA DE DATOS
+##################
+
+dataPath = "./datos/regresion/YearPredictionMSD.txt"
+
+musicData = loadData(dataPath, ",")
+
+#%% CODIFICAR LOS DATOS
+#######################
+
+#%%
+
+X, Y = charsTagsSplit(musicData, 0)
+
+train_chars, test_chars, train_tags, test_tags = skml.train_test_split(X, Y, test_size=0.3)
+
+normalize = np.arange(1, 91)
+normalizeData(train_chars, test_chars, normalize)
+
+#%%
+
+train_x = train_chars.to_numpy()
+train_y = train_tags.to_numpy()
+test_x = test_chars.to_numpy()
+test_y = test_tags.to_numpy()
