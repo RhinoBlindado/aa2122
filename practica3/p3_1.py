491718972b52d89d226a830f12c1346cb5c44630
Valentino Lugli <valentinolugli@gmail.com>
2022-06-05 22:36:34 +0200

    [P3][100%] This shit bussin fr fr no cap

--- practica3/p3_1.py
+++ practica3/p3_1.py
@@ -32,33 +32,143 @@ from tabulate import tabulate as tb
 # Fijando la semilla
 np.random.seed(1995)
 
+## FUNCIONES AUXILIARES
+
+def stop():
+    """
+    Detiene la ejecución hasta que se presione 'Enter'
+
+    """
+    input("Presiona Enter para continuar. . .")
+
+def genBox(string):
+    """
+    Genera una caja de al rededor del texto para que se vea más estético.
+
+    """
+    strLen = len(string) + 2
+    
+    print("+",end='')
+    for i in range(1, strLen - 1):
+        print("-",end='')
+    print("+")
+    
+    print("|{}|".format(string))
+    
+    print("+",end='')
+    for i in range(1, strLen - 1):
+        print("-",end='')
+    print("+\n")
+
 # FUNCIONES PARA EJERCICIOS
 
 def encodeCycle(data, col, maxVal):
+    """
+    Codificar los datos en seno-coseno.
+
+    Parameters
+    ----------
+    data : Pandas Dataframe
+        Los datos a modificar
+    col : String
+        La columna a modificar.
+    maxVal : Float/Int
+        El valor máximo que puede tener la variable cíclica.
+
+    Returns
+    -------
+    None.
+
+    """
     data[col+"_sin"] = np.sin(2 * np.pi * data[col] / maxVal)
     data[col+"_cos"] = np.cos(2 * np.pi * data[col] / maxVal)
         
 def monthToInt(data, col, monthDict):
+    """
+    Convertir los meses de cadena a entero.
+
+    Parameters
+    ----------
+    data : Pandas Dataframe
+        Los datos a modificar.
+    col : String
+        El nombre de la columna a modificar.
+    monthDict : Diccionario.
+        Diccionario que mapea las cadenas de los meses a su valor entero.
+
+    Returns
+    -------
+    None.
+
+    """
     data[col] = data[col].map(monthDict)
 
 def loadData(dataPath, sep=";", header=0):
+    """
+    Cargar los datos a un Dataframe de Pandas.
+
+    Parameters
+    ----------
+    dataPath : String
+        Ruta hacia el fichero con los datos.
+    sep : String, optional
+        Separador de los datos, por defecto es ";".
+    header : String, optional
+        Como tratar la cabecera del fichero. Por defecto es 0.
+
+    Returns
+    -------
+    dataset : TYPE
+        DESCRIPTION.
+
+    """
     dataset = pd.read_csv(dataPath, sep=sep, header=header)
     return dataset
 
 def processData(data, categorical, binary, cyclical, custom):
+    """
+    Procesar los datos de un Dataframe
+
+    Parameters
+    ----------
+    data : Pandas Dataframe
+        Los datos originales.
+    categorical : Lista.
+        Lista con las variables categóricas para aplicarles One-Hot Encoding.
+    binary : Lista.
+        Lista con las variables binarias para cambiarlas de 'yes' y 'no' a -1, 1.
+    cyclical : Lista de diccionario.
+        Lista con las variables cíclicas para cambiarlas a codificación seno-coseno.
+    custom : Lista de diccionario.
+        Lista con variables y su mapeo particular.
+
+    Returns
+    -------
+    data : Pandas Dataframe
+        Datos modificados
+
+    """
     
+    # Si se han pasado variables categóricas...
     if categorical is not None:
+        # ... utilizar la función de dummies apra el One-Hot Encoding.
         data = pd.get_dummies(data, columns=categorical, drop_first=True)
     
+    # Si se han pasado binarias...
     if binary is not None:
         for feat in binary:
+            # ... convertir de 'yes' y 'no' a '1' y '-1'.
             data[feat] = np.where(data[feat] == 'yes', 1, -1)
-     
+    
+    # Ídem con cíclicas...
     if cyclical is not None:
         for feat in cyclical:
+            # Codificar en seno-coseno.
             encodeCycle(data, feat, cyclical[feat])
+            # Una vez se ha convertido, se quita la columna original.
             data.drop(labels=[feat], axis=1, inplace=True)
-
+    
+    # Ídem con mapeados personalizados...
     if custom is not None:
         for feat in custom:
             for key in feat:
@@ -67,21 +177,67 @@ def processData(data, categorical, binary, cyclical, custom):
     return data
 
 def charsTagsSplit(data, tag):
+    """
+    Dividir los datos en las características X y las etiquetas Y.
+
+    Parameters
+    ----------
+    data : Pandas Dataframe
+        Los datos enteros.
+    tag : String
+        Nombre de la etiqueta del problema.
+
+    Returns
+    -------
+    x : Pandas Dataframe
+        Características.
+    y : Pandas Dataframe
+        Etiqueta.
+
+    """
     x = data.drop([tag], axis=1)
     y = data[tag]
     
     return x, y
 
 def normalizeData(train, test, normCols, featRange=((-1, 1))):
+    """
+    Normalizar los datos de entrenamiento y test, escalar los valores a valores
+    entre -1 y 1.
+
+    Parameters
+    ----------
+    train : Pandas Dataframe
+        Características del conjunto de entrenamiento.
+    test : Pandas Dataframe
+        Características del conjunto de test.
+    normCols : List
+        Lista con los nombres de las columnas a escalar.
+    featRange : Dupla, opcional.
+        Dupla con los valorea a los que escalar los datos. Por defecto, ((-1, 1)).
+
+    Returns
+    -------
+    None.
+
+    """
+    # Obtener el objeto que escala con los rangos.
     scaler = skpp.MinMaxScaler(feature_range=featRange)
 
+    # Ajustar el objeto con los valores de entrenamiento.
     scaler.fit(train[normCols])
+    
+    # Con estos valores, escalar los datos de entrenamiento y test.
     train[normCols] = scaler.transform(train[normCols])
     test[normCols] = scaler.transform(test[normCols])
 
 
 def printLabelFrec(df, column = None, testOverride = False, decimals=".2f"):
-    
+    """
+    Función auxiliar para obtener las frecuencias de los distintos atributos
+    de las variables categóricas.
+
+    """
     if not testOverride:
         labels = np.unique(df[column])
         total = len(df[column])   
@@ -107,11 +263,18 @@ def printLabelFrec(df, column = None, testOverride = False, decimals=".2f"):
     
 
 def plotLabelFrec(labels, frecs, rotation="45", title=""):
+    """
+    Función auxiliar para mostrar en un histograma la frecuencia de unas 
+    etiquetas categóricas
+
+    """
     plt.figure()
     
+    # Calcular el color dependiendo de la frecuencia del atributo.
     scaleColor = lambda a, top : a / top 
     actColors = cm.rainbow(scaleColor(frecs, np.max(frecs)))
     
+    # Dibujarlo.
     plt.bar(labels, frecs, color=actColors)       
     plt.ylabel("Proporción en muestra")
     plt.xlabel("Etiquetas")
@@ -122,8 +285,13 @@ def plotLabelFrec(labels, frecs, rotation="45", title=""):
     
     
 def plotLearningCurve(sizes, train, test, title=None):
+    """
+    Función auxiliar para dibujar las curvas de aprendizaje
+
+    """
     plt.figure()
     
+    # Obtener los valores medios, que son los que serán dibujados.
     train = np.mean(train, axis=1)
     test  = np.mean(test, axis=1)
     
@@ -144,6 +312,8 @@ def plotLearningCurve(sizes, train, test, title=None):
 
 #%% CARGA DE DATOS
 ##################
+
+genBox("Carga de datos")
 
 # Se definen diferentes listas con los atributos del dataset, identificando...
 
@@ -179,6 +349,8 @@ bankData.drop(labels=["duration"], axis=1, inplace=True)
 #%% DIVISION DE DATOS
 #####################
 
+genBox("División de Datos")
+
 # La columna a predecir es la 'y', se separa del resto de datos.
 X, Y = charsTagsSplit(bankData, 'y')
 
@@ -189,41 +361,55 @@ X, Y = charsTagsSplit(bankData, 'y')
 train_chars, test_chars, train_tags, test_tags = skms.train_test_split(X, 
                                                                        Y, 
                                                                        stratify=Y,
-                                                                       test_size=0.3)
+                                                                       test_size=0.3,
+                                                                       random_state=16)
 
 #%% ANALISIS DE DATOS
 #####################
 
-print("Análisis de Datos:")
-print("Mostrando diferentes estadísticas del conjunto de entrenamiento:")
+genBox("Análisis de Datos")
+print("- Mostrando diferentes estadísticas del conjunto de entrenamiento:")
 with pd.option_context('display.max_columns', 32):
-    print(train_chars.describe(include=np.number))
-    print(train_chars.describe(exclude=np.number))
+    print("-- Variables continuas")
+    print(train_chars.describe(include=np.number).round(decimals=2))
+    print("-- Variables categóricas")
+    print(train_chars.describe(exclude=np.number).round(decimals=2))
     print("\n")
 
-print("Variables categóricas en más detalle:")
+print("- Variables categóricas en más detalle:\n")
 for i in categorical:
     print("Columna '{}'".format(i))
     labels, frecs = printLabelFrec(train_chars, i)
-    plotLabelFrec(labels, frecs, title="Atributo {}".format(i))
+    plotLabelFrec(labels, frecs, title="Atributo {}".format(i),rotation="vertical")
     print("\n")
     
     
-print("Variables binarias en más detalle:")
+print("- Variables binarias en más detalle:\n")
 for i in binary:
-    print("Columna '{}'".format(i))
+    print("-- Columna '{}'".format(i))
     labels, frecs = printLabelFrec(train_chars, i)
     plotLabelFrec(labels, frecs, title="Atributo '{}'".format(i))
     print("\n")
     
     
-print("Columna 'y'")
+print("- Variables cíclicas en más detalle:\n")
+for i in ["month", "poutcome"]:
+    print("-- Columna '{}'".format(i))
+    labels, frecs = printLabelFrec(train_chars, i)
+    plotLabelFrec(labels, frecs, title="Atributo '{}'".format(i))
+    print("\n")
+    
+    
+print("-- Columna 'y'")
+train_tags.describe()
 labels, frecs = printLabelFrec(train_tags, testOverride=True)
 plotLabelFrec(labels, frecs, title="Atributo 'y'")
 
 
 #%% CODIFICAR LOS DATOS
 #######################
+
+genBox("Codificación de datos")
 
 # Convertir los meses a sus valores enteros entre 1 y 12.
 monthToInt(train_chars, "month", monthDict)
@@ -249,19 +435,26 @@ test_y = np.where(test_tags[:] == 'yes', 1, -1)
 #%% VALIDACION CRUZADA
 ######################
 
+genBox("Validación Cruzada")
+
+# Definir los parámetros que se desean explorar en el espacio de modelos de
+# regresión logística.
 
 CVParamGrid =   [{
-                    'loss' : ['log'],
                     'class_weight': [None, 'balanced'],
                     'alpha': [0.0001, 0.001, 0.01],
                     'eta0' : [0.01, 0.1, 0.3],
                     'learning_rate' : ['adaptive','optimal'],
                 }]
 
+# La forma que se desea valorar la calidad de la validación cruzada.
+scores = ["f1"]
 
-scores = ["accuracy", "precision", "recall", "f1"]
+print("Iniciando...")
 
-RLGrid = skms.GridSearchCV(sklm.SGDClassifier(), 
+# Definir el objeto, se indican más atributos que ya se fijan de una vez.
+
+RLGrid = skms.GridSearchCV(sklm.SGDClassifier(loss='log', penalty='l1', max_iter=10000), 
                        CVParamGrid, 
                        scoring=scores,
                        refit="f1",
@@ -269,18 +462,25 @@ RLGrid = skms.GridSearchCV(sklm.SGDClassifier(),
                        verbose=1,
                        n_jobs=-1)
 
+# Ajustar los datos.
 RLGrid.fit(train_x, train_y)
 
-print("Resultados de Cross-Validation:")
-print("Mejor puntuación F1: {}".format(RLGrid.best_score_))
+# Obtener el mejor modelo.
+bestRL = RLGrid.best_estimator_
+
+print("Resultados:")
+print("Mejor Puntuación Media de F1: {}".format(RLGrid.best_score_))
+print("Error Medio de Entropía Cruzada: {}".format(skm.log_loss(train_y, bestRL.predict(train_x))))
 print("Mejores parámetros: {}".format(RLGrid.best_params_))
 
 #%% ENTRENAMIENTO
 #################
 
-bestRL = RLGrid.best_estimator_
+genBox("Entrenamiento")
 
+# Reentrenar el mejor modelo con todos los datos.
 bestRL.fit(train_x, train_y)
+
 pred_train_y = bestRL.predict(train_x)
 
 print("Conjunto de Entrenamiento:")
@@ -290,6 +490,8 @@ print("F1: {:.4f}".format(skm.f1_score(train_y, pred_train_y)))
 
 #%% TEST
 ########
+
+genBox("Test")
 
 pred_test_y = bestRL.predict(test_x)
 
@@ -302,8 +504,10 @@ print("F1: {:.4f}".format(skm.f1_score(test_y, pred_test_y)))
 #%% ANALISIS DE RESULTADOS
 ##########################
 
+genBox("Análisis de Resultados")
+
 print("Matriz de Confusión: \n")
-print(skm.confusion_matrix(test_y, pred_test_y), "\n")
+print(skm.confusion_matrix(test_y, pred_test_y, labels=[1,-1]), "\n")
 
 print("Reporte de clasificación:")
 print(skm.classification_report(test_y, pred_test_y))
@@ -311,6 +515,8 @@ print(skm.classification_report(test_y, pred_test_y))
 
 #%% CURVAS DE APRENDIZAJE
 #########################
+
+genBox("Curvas de Aprendizaje")
 
 trainSizes, trainScore, testScore = skms.learning_curve(bestRL, 
                                                         train_x, 
@@ -322,12 +528,15 @@ trainSizes, trainScore, testScore = skms.learning_curve(bestRL,
 
 plotLearningCurve(trainSizes, trainScore, testScore, title="F1: Curva de aprendizaje")
 
+# Se convierte de métrica de error para poder realizar la curva de aprendizaje 
+logScorer = skm.make_scorer(skm.log_loss, greater_is_better=False)
+
 trainSizes, trainScore, testScore = skms.learning_curve(bestRL, 
                                                         train_x, 
                                                         train_y,
                                                         cv=10,
-                                                        scoring="accuracy",
+                                                        scoring=logScorer,
                                                         train_sizes=np.linspace(0.1, 1.0, 15),
                                                         n_jobs=-1)
 
-plotLearningCurve(trainSizes, trainScore, testScore, title="Accuracy: Curva de aprendizaje")
+plotLearningCurve(trainSizes, trainScore, testScore, title="Entropía Cruzada: Curva de aprendizaje")
--- practica3/p3_2.py
+++ practica3/p3_2.py
@@ -32,33 +32,143 @@ from tabulate import tabulate as tb
 # Fijando la semilla
 np.random.seed(1995)
 
+## FUNCIONES AUXILIARES
+
+def stop():
+    """
+    Detiene la ejecución hasta que se presione 'Enter'
+
+    """
+    input("Presiona Enter para continuar. . .")
+
+def genBox(string):
+    """
+    Genera una caja de al rededor del texto para que se vea más estético.
+
+    """
+    strLen = len(string) + 2
+    
+    print("+",end='')
+    for i in range(1, strLen - 1):
+        print("-",end='')
+    print("+")
+    
+    print("|{}|".format(string))
+    
+    print("+",end='')
+    for i in range(1, strLen - 1):
+        print("-",end='')
+    print("+\n")
+
 # FUNCIONES PARA EJERCICIOS
 
 def encodeCycle(data, col, maxVal):
+    """
+    Codificar los datos en seno-coseno.
+
+    Parameters
+    ----------
+    data : Pandas Dataframe
+        Los datos a modificar
+    col : String
+        La columna a modificar.
+    maxVal : Float/Int
+        El valor máximo que puede tener la variable cíclica.
+
+    Returns
+    -------
+    None.
+
+    """
     data[col+"_sin"] = np.sin(2 * np.pi * data[col] / maxVal)
     data[col+"_cos"] = np.cos(2 * np.pi * data[col] / maxVal)
         
 def monthToInt(data, col, monthDict):
+    """
+    Convertir los meses de cadena a entero.
+
+    Parameters
+    ----------
+    data : Pandas Dataframe
+        Los datos a modificar.
+    col : String
+        El nombre de la columna a modificar.
+    monthDict : Diccionario.
+        Diccionario que mapea las cadenas de los meses a su valor entero.
+
+    Returns
+    -------
+    None.
+
+    """
     data[col] = data[col].map(monthDict)
 
 def loadData(dataPath, sep=";", header=0):
+    """
+    Cargar los datos a un Dataframe de Pandas.
+
+    Parameters
+    ----------
+    dataPath : String
+        Ruta hacia el fichero con los datos.
+    sep : String, optional
+        Separador de los datos, por defecto es ";".
+    header : String, optional
+        Como tratar la cabecera del fichero. Por defecto es 0.
+
+    Returns
+    -------
+    dataset : TYPE
+        DESCRIPTION.
+
+    """
     dataset = pd.read_csv(dataPath, sep=sep, header=header)
     return dataset
 
 def processData(data, categorical, binary, cyclical, custom):
+    """
+    Procesar los datos de un Dataframe
+
+    Parameters
+    ----------
+    data : Pandas Dataframe
+        Los datos originales.
+    categorical : Lista.
+        Lista con las variables categóricas para aplicarles One-Hot Encoding.
+    binary : Lista.
+        Lista con las variables binarias para cambiarlas de 'yes' y 'no' a -1, 1.
+    cyclical : Lista de diccionario.
+        Lista con las variables cíclicas para cambiarlas a codificación seno-coseno.
+    custom : Lista de diccionario.
+        Lista con variables y su mapeo particular.
+
+    Returns
+    -------
+    data : Pandas Dataframe
+        Datos modificados
+
+    """
     
+    # Si se han pasado variables categóricas...
     if categorical is not None:
+        # ... utilizar la función de dummies apra el One-Hot Encoding.
         data = pd.get_dummies(data, columns=categorical, drop_first=True)
     
+    # Si se han pasado binarias...
     if binary is not None:
         for feat in binary:
+            # ... convertir de 'yes' y 'no' a '1' y '-1'.
             data[feat] = np.where(data[feat] == 'yes', 1, -1)
-     
+    
+    # Ídem con cíclicas...
     if cyclical is not None:
         for feat in cyclical:
+            # Codificar en seno-coseno.
             encodeCycle(data, feat, cyclical[feat])
+            # Una vez se ha convertido, se quita la columna original.
             data.drop(labels=[feat], axis=1, inplace=True)
-
+    
+    # Ídem con mapeados personalizados...
     if custom is not None:
         for feat in custom:
             for key in feat:
@@ -67,21 +177,67 @@ def processData(data, categorical, binary, cyclical, custom):
     return data
 
 def charsTagsSplit(data, tag):
+    """
+    Dividir los datos en las características X y las etiquetas Y.
+
+    Parameters
+    ----------
+    data : Pandas Dataframe
+        Los datos enteros.
+    tag : String
+        Nombre de la etiqueta del problema.
+
+    Returns
+    -------
+    x : Pandas Dataframe
+        Características.
+    y : Pandas Dataframe
+        Etiqueta.
+
+    """
     x = data.drop([tag], axis=1)
     y = data[tag]
     
     return x, y
 
 def normalizeData(train, test, normCols, featRange=((-1, 1))):
+    """
+    Normalizar los datos de entrenamiento y test, escalar los valores a valores
+    entre -1 y 1.
+
+    Parameters
+    ----------
+    train : Pandas Dataframe
+        Características del conjunto de entrenamiento.
+    test : Pandas Dataframe
+        Características del conjunto de test.
+    normCols : List
+        Lista con los nombres de las columnas a escalar.
+    featRange : Dupla, opcional.
+        Dupla con los valorea a los que escalar los datos. Por defecto, ((-1, 1)).
+
+    Returns
+    -------
+    None.
+
+    """
+    # Obtener el objeto que escala con los rangos.
     scaler = skpp.MinMaxScaler(feature_range=featRange)
 
+    # Ajustar el objeto con los valores de entrenamiento.
     scaler.fit(train[normCols])
+    
+    # Con estos valores, escalar los datos de entrenamiento y test.
     train[normCols] = scaler.transform(train[normCols])
     test[normCols] = scaler.transform(test[normCols])
 
 
 def printLabelFrec(df, column = None, testOverride = False, decimals=".2f"):
-    
+    """
+    Función auxiliar para obtener las frecuencias de los distintos atributos
+    de las variables categóricas.
+
+    """
     if not testOverride:
         labels = np.unique(df[column])
         total = len(df[column])   
@@ -107,11 +263,18 @@ def printLabelFrec(df, column = None, testOverride = False, decimals=".2f"):
     
 
 def plotLabelFrec(labels, frecs, rotation="45", title=""):
+    """
+    Función auxiliar para mostrar en un histograma la frecuencia de unas 
+    etiquetas categóricas
+
+    """
     plt.figure()
     
+    # Calcular el color dependiendo de la frecuencia del atributo.
     scaleColor = lambda a, top : a / top 
     actColors = cm.rainbow(scaleColor(frecs, np.max(frecs)))
     
+    # Dibujarlo.
     plt.bar(labels, frecs, color=actColors)       
     plt.ylabel("Proporción en muestra")
     plt.xlabel("Etiquetas")
@@ -122,8 +285,13 @@ def plotLabelFrec(labels, frecs, rotation="45", title=""):
     
     
 def plotLearningCurve(sizes, train, test, title=None):
+    """
+    Función auxiliar para dibujar las curvas de aprendizaje
+
+    """
     plt.figure()
     
+    # Obtener los valores medios, que son los que serán dibujados.
     train = np.mean(train, axis=1)
     test  = np.mean(test, axis=1)
     
@@ -142,8 +310,11 @@ def plotLearningCurve(sizes, train, test, title=None):
 
 # IMPLEMENTACION EJERCICIOS
 
+
 #%% CARGA DE DATOS
 ##################
+
+genBox("Carga de datos")
 
 dataPath = "./datos/regresion/YearPredictionMSD.txt"
 
@@ -152,33 +323,49 @@ musicData = loadData(dataPath, ",", None)
 #%% DIVISION DE DATOS
 #####################
 
+genBox("División de Datos")
+
+# En la columna 0 están las etiquetas.
 X, Y = charsTagsSplit(musicData, 0)
 
-train_chars, test_chars, train_tags, test_tags = skms.train_test_split(X, Y, test_size=0.3)
+# Realizar el split de entrenamiento y test.
+train_chars, test_chars, train_tags, test_tags = skms.train_test_split(X, 
+                                                                       Y, 
+                                                                       test_size=0.3)
 
 
 #%% ANALISIS DE DATOS
 #####################
 
+genBox("Análisis de datos")
+
 print("Análisis de Datos:")
 print("Mostrando diferentes estadísticas del conjunto de entrenamiento:")
 with pd.option_context('display.max_columns', 8):
-    print(train_chars.describe(include=np.number))
+    print(train_chars.describe(include=np.number).round(decimals=2))
     print("\n")
 
 
 print("Columna 'año'")
-print(train_tags.describe())
+print(train_tags.describe().round(decimals=2))
 labels, frecs = printLabelFrec(train_tags, testOverride=True, decimals=".4f")
 plotLabelFrec(labels, frecs, title="Atributo 'año'")
 
 #%% CODIFICAR LOS DATOS
 #######################
 
+# Como las variables van de 1 a 90, se saca un vector de [1, 2, ..., 90] 
+# para escalar los valores al rango [-1, 1]
+normalize = np.arange(1, 91)
+normalizeData(train_chars, test_chars, normalize)
+
+# Aplicar PCA y quedarse con el 95% de la varianza.
 pca = skd.PCA(n_components=0.95)
 
+# Usar los datos de entrenamiento para el fit.
 pca.fit(train_chars)
 
+# Y transformar los de entrenamiento y test por igual.
 red_train_x = pca.transform(train_chars)
 red_test_x  = pca.transform(test_chars)
 
@@ -190,17 +377,24 @@ test_y = test_tags.to_numpy()
 #%% VALIDACION CRUZADA
 ######################
 
+genBox("Validación Cruzada")
+
+# Se definen los parámetros a explorar con regresión lineal.
+
 CVParamGrid =   [{
-                    'loss' : ['squared_error'],
                     'alpha': [0.001, 0.01],
                     'eta0' : [0.01, 0.1],
                     'learning_rate' : ['adaptive','optimal'],
                 }]
 
-
+# La forma que se desea valorar la calidad de la validación cruzada.
 score = "r2"
 
-LRGrid = skms.GridSearchCV(sklm.SGDRegressor(max_iter=10000), 
+# Definir el objeto, se indican más atributos que ya se fijan de una vez.
+
+LRGrid = skms.GridSearchCV(sklm.SGDRegressor(loss="squared_error", 
+                                             penalty="l2",
+                                             max_iter=10000), 
                        CVParamGrid, 
                        scoring=score,
                        refit="r2",
@@ -212,37 +406,46 @@ LRGrid = skms.GridSearchCV(sklm.SGDRegressor(max_iter=10000),
 print("Empezando la selección de mejor modelo con Cross Validation")
 print("Tiempo estimado: 3 minutos\n")
 
+# Ajustar los datos...
 LRGrid.fit(red_train_x, train_y)
+
+# ... obtener el modelo que tuvo la mejor puntuación de CV.
+bestLR = LRGrid.best_estimator_
 
 print("Resultados de Cross-Validation:")
 print("Mejor puntuación R^2: {}".format(LRGrid.best_score_))
+print("MSE: {}".format(skm.mean_squared_error(train_y, bestLR.predict(red_train_x))))
 print("Mejores parámetros: {}".format(LRGrid.best_params_))
 
 
 #%% ENTRENAMIENTO
 #################
 
-bestLR = LRGrid.best_estimator_
+genBox("Entrenamiento")
 
 bestLR.fit(red_train_x, train_y)
 pred_train_y = bestLR.predict(red_train_x)
 
 print("Conjunto de Entrenamiento:")
 print("Error Cuadrático Medio: {:.4f}".format(skm.mean_squared_error(train_y, pred_train_y)))
-print("Error R^2: {:.4f}".format(bestLR.score(red_train_x, train_y)))
+print("Coeficiente R^2: {:.4f}".format(bestLR.score(red_train_x, train_y)))
 
 #%% TEST
 ########
+
+genBox("Test")
 
 pred_test_y = bestLR.predict(red_test_x)
 
 print("Conjunto de Test:")
 print("Error Cuadrático Medio: {:.4f}".format(skm.mean_squared_error(test_y, pred_test_y)))
-print("Error R^2: {:.4f}".format(bestLR.score(red_test_x, test_y)))
+print("Coeficiente R^2: {:.4f}".format(bestLR.score(red_test_x, test_y)))
 
 
 #%% CURVAS DE APRENDIZAJE
 #########################
+
+genBox("Curvas de Aprendizaje")
 
 trainSizes, trainScore, testScore = skms.learning_curve(bestLR, 
                                                         red_train_x, 
